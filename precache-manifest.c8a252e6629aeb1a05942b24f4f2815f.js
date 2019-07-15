70f088ba6b30e2e334a5195b23daa234bdb632fd
Juliette Audema <juliette.audema@gmail.com>
2019-07-15 12:59:42 +0200

    Updates

---
+++ precache-manifest.c8a252e6629aeb1a05942b24f4f2815f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7e078db68e9d4bc2da5ce1f88503d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e601a1c4a0b56ebf383d",
+    "url": "/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "e601a1c4a0b56ebf383d",
+    "url": "/static/js/main.3424b6ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
